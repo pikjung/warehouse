d637266e24e805615877ae23f3e9b95885f5fe00
fikritanjung <tanjungfikri1@gmail.com>
2021-03-22 13:55:27 +0700

    Adding gudang view in controller to return data to views

--- app/Http/Controllers/gudangController.php
+++ app/Http/Controllers/gudangController.php
@@ -4,7 +4,41 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\gudang;
+
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\log;
+
+use Illuminate\Support\Facades\Validator;
+
+use DataTables;
+
+use DB;
+
 class gudangController extends Controller
 {
     //
+
+    public function gudang()
+    {
+        return view('/gudang/gudang');
+    }
+
+    public function gudangView()
+    {
+        $data = gudang::orderBy('created_at','desc')->get();
+        return Datatables::of(gudang::orderBy('created_at','desc'))
+        ->addColumn('action', function ($data)
+        {
+            return '<a href="#" id="edit_gudang" onclick=edit_gudang("'.$data->gudang_id.'") class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-edit"></i></a><a href="#" id="hapus_gudang" onclick=hapus_gudang("'.$data->gudang_id.'") class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-trash"></i></a>';
+            
+        })
+        ->make(true);
+    }
+
+    public function gudangTambah(Request $request)
+    {
+        
+    }
 }
