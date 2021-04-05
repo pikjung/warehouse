46b5f02f35b292ee1cb6c902673e8d586b5174c1
fikritanjung <tanjungfikri1@gmail.com>
2021-04-05 09:49:43 +0700

    use data toko model in controller, add method data toko view and data tables

--- app/Http/Controllers/storeController.php
+++ app/Http/Controllers/storeController.php
@@ -4,7 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\data_toko;
+
 class storeController extends Controller
 {
-    //
+    //data toko
+
+    //view data toko
+    public function data_toko()
+    {
+        return view('/store/data_toko/data_toko');
+    }
+
+    //dataTables data toko
+    public function data_tokoView()
+    {
+        $data1 = expedisi::all();
+        return Datatables::of(expedisi::all())
+        ->addColumn('action', function ($data1)
+        {
+             return '<a href="#" id="edit_expedisi" onclick=edit_expedisi("'.$data1->expedisi_id.'") class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-edit"></i></a><a href="#" id="hapus_expedisi" onclick=hapus_expedisi("'.$data1->expedisi_id.'") class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-trash"></i></a>';
+        })
+        ->editcolumn('detail', function ($data1)
+        {
+            return '<a href="#" class="btn btn-info" onclick=detail_expedisi("'.$data1->expedisi_id.'")><i class="glyphicon glyphicon-eye-open"></i></a>';
+        })
+        ->rawColumns(['action','detail'])
+        ->make(true);
+    }
 }
--- resources/views/store/data_toko/data_toko.blade.php
+++ resources/views/store/data_toko/data_toko.blade.php
@@ -5,7 +5,7 @@
         <div class="col-md-12 col-sm-12 ">
             <div class="x_panel">
               <div class="x_title">
-                <h2> Expedisi <small>Table</small></h2>
+                <h2> Data Toko <small>Table</small></h2>
                 <ul class="nav navbar-right panel_toolbox">
                   <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                   </li>
